7ed97d6ae9bbaeeeb7bec97cc6b6f5806f65e03b
Kwon_Gwan <kkh3486@naver.com>
2025-11-26 16:01:55 +0900

    20251126 권구환 IOS ARFoundation 사용 기초 틀 작성

---
+++ Assets/#KKH/Scripts/ARKit/ArKitBlendshapeToVRM.cs
@@ -0,0 +1,73 @@
+﻿using Unity.Collections;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARKit;
+using UniVRM10;
+using VRM;
+
+public class ARKitBlendshapeToVRM : MonoBehaviour
+{
+    [Header("AR Foundation")]
+    public ARFaceManager faceManager;
+    public ARFace arFace;
+
+    [Header("VRM10")]
+    public Vrm10Instance vrm10Instance;
+
+
+    // VRM10 Expression Keys
+    ExpressionKey _blinkL = ExpressionKey.BlinkLeft;
+    ExpressionKey _blinkR = ExpressionKey.BlinkRight;
+    ExpressionKey _aa = ExpressionKey.Aa;
+    ExpressionKey _happy = ExpressionKey.Happy;
+
+    void Update()
+    {
+#if UNITY_IOS && !UNITY_EDITOR
+if (arFace == null || vrm10Instance == null || faceManager == null) return;
+
+        var subsystem = faceManager.subsystem as ARKitFaceSubsystem;
+        if(subsystem == null) return;
+
+        using(var coeffs = subsystem.GetBlendShapeCoefficients(arFace.trackableId, Allocator.Temp))
+        {
+        var exp = vrm10Instance.Runtime.Expression;
+            float blinkL = 0f;
+            float blinkR = 0f;
+            float jawOpen = 0f;
+            float smileL = 0f;
+            float smileR = 0f;
+
+            foreach (var c in coeffs)
+            {
+                switch (c.blendShapeLocation)
+                {
+                    case ARKitBlendShapeLocation.EyeBlinkLeft:
+                        blinkL = c.coefficient;
+                        break;
+
+                    case ARKitBlendShapeLocation.EyeBlinkRight:
+                        blinkR = c.coefficient;
+                        break;
+
+                    case ARKitBlendShapeLocation.JawOpen:
+                        jawOpen = c.coefficient;
+                        break;
+
+                    case ARKitBlendShapeLocation.MouthSmileLeft:
+                        smileL = c.coefficient;
+                        break;
+
+                    case ARKitBlendShapeLocation.MouthSmileRight:
+                        smileR = c.coefficient;
+                        break;
+                }
+            }
+            exp.SetWeight(_blinkL, blinkL);
+            exp.SetWeight(_blinkR, blinkR);
+            exp.SetWeight(_aa, jawOpen);
+            exp.SetWeight(_happy, (smileL + smileR) * 0.5f);
+        }
+#endif
+    }
+}
